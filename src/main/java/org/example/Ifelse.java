64d16d7307e712f42a2764fb856f4c70c1a8e8a4
Harshvs1857 <harshvs1857@gmail.com>
2024-01-11 17:07:35 +0530

    maven project

---
+++ src/main/java/org/example/Ifelse.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Ifelse {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        if (a + b == 6)
+        {
+            System.out.println("True");
+        } else
+        {
+            System.out.println("False");
+        }
+
+    }
+}
+
+
+
+
+
+
+
