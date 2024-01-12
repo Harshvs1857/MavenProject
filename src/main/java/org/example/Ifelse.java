c3a58c9ed901e05e91a9e5e4e2e535403499dddb
Harshvs1857 <harshvs1857@gmail.com>
2024-01-13 00:31:41 +0530

    First change

--- src/main/java/org/example/Ifelse.java
+++ src/main/java/org/example/Ifelse.java
@@ -3,18 +3,16 @@ package org.example;
 public class Ifelse {
 
     public static void main(String[] args) {
-        int a = 1;
-        int b = 5;
-        if (a + b == 6)
-        {
-            System.out.println("True");
-        } else
-        {
-            System.out.println("False");
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
         }
 
     }
-}
+
 
 
 
